07fb343ecd616d711e4c8a462413421e98402ee1
Francia Csaba <h157595@stud.u-szeged.hu>
2023-10-27 21:49:44 +0200

    Create tables

---
+++ database/migrations/2023_10_26_140327_create_translations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade'); // Cascade delete on book deletion
+            $table->foreignId('language_id')->constrained()->onDelete('cascade'); // Cascade delete on language deletion
+            $table->string('title', 80);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('translations');
+    }
+};
