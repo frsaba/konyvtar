00a25af478cf455077c16abd534590deb04798fd
Francia Csaba <h157595@stud.u-szeged.hu>
2023-10-28 23:43:57 +0200

    Filter books based on title or authors

--- resources/js/Pages/Books.vue
+++ resources/js/Pages/Books.vue
@@ -6,6 +6,31 @@ export default {
 	props: {
 		books: Array,
 	},
+	data:() =>({
+		searchTitle: "",
+		searchAuthors: ""
+	}),
+	computed: {
+		filteredBooks(){
+			let books = this.books;
+			if(this.searchTitle) 
+				books = books.filter(b => this.matches(this.getBookTitle(b),this.searchTitle))
+			if(this.searchAuthors)
+				books = books.filter(b => this.matches(this.getBookAuthors(b),this.searchAuthors))
+			return books;
+		},
+	},
+	methods:{
+		getBookTitle(book){
+			return book.translations[0]?.title
+		},
+		getBookAuthors(book) {
+			return book.authors?.map(author => author?.name).join(", ");
+		},
+		matches(str,query){
+			return str.toLowerCase().includes(query.toLowerCase())
+		}
+	}
 }
 </script>
 
@@ -17,18 +42,20 @@ export default {
 			<h1>Search books</h1>
 			<v-text-field
 				name="title_search"
+				v-model.trim="searchTitle"
 				label="Search in title"
 			></v-text-field>
 
 			<v-text-field
 				name="author_search"
 				label="Search authors"
+				v-model.trim="searchAuthors"
 			></v-text-field>
 
 		</div>
 
 		<div class="books">
-			<book-card v-for="book in books ?? []" :key="book.isbn" v-bind="book">
+			<book-card v-for="book in filteredBooks ?? []" :key="book.isbn" v-bind="book">
 
 			</book-card>
 		</div>
